76cebc99838874ff048e29d9c42ecc96340a2708
Chrescht1337 <frantzenchristian@hotmail.com>
2014-12-10 10:57:40 +0100

    little refactoring

--- Node.java
+++ Node.java
@@ -1,30 +1,33 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
-public class Node
-{
+public class Node{
   private String status;
   // ' ' pour libre, 'P' pour Pakkuman, 'B' pour bonbons,'M' pour monstres
   // '#' pour traversé, 'S' pour la sortie
   private List<Node> neighbours;
 
 
-  public Node()
-  {
+  public Node(){
     this.status =" ";
     this.neighbours= new ArrayList<Node>();
   }
-  public void addNeighbour(Node n)
-  {
-    if (!(this.neighbours.contains(n)))
-    {
+  public void addNeighbour(Node n){
+    if (!(this.neighbours.contains(n))){
       this.neighbours.add(n);
       n.addNeighbour(this);
     }
   }
 
-  public void setStatus(String status)
-  {
+  public void setStatus(String status){
     this.status=status;
   }
+
+  public int getNbrOfNeighbours(){
+    return this.neighbours.size();
+  }
+
+  //public void toString(){
+
+  //}
 }
